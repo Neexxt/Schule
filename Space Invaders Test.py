b4e4aaacff15c03b563ff3df9f196cf6720e3bd7
Jayson <84500156+Neexxt@users.noreply.github.com>
2021-10-07 14:15:48 +0200

    Update Space Invaders Test.py

--- Space Invaders Test.py
+++ Space Invaders Test.py
@@ -7,6 +7,9 @@ screen_width = 1200
 screen_height = 550
 player_spaceship = pygame.image.load("spaceship.png")
 game_background = pygame.image.load("space background.png")
+laser_png = pygame.image.load("Fireball1.png")
+game_background = pygame.transform.scale(game_background, (1200, 550))
+laser_png = pygame.transform.scale(laser_png, (64, 64))
 
 
 class space_ship(pygame.sprite.Sprite):
@@ -16,6 +19,7 @@ class space_ship(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.centerx = screen_width / 2
         self.rect.bottom = screen_height / 1.1
+        self.pos = self.rect.centerx, self.rect.bottom
         self.speed_ship = 0
 
     def update(self):
@@ -32,8 +36,13 @@ class space_ship(pygame.sprite.Sprite):
             self.rect.left = 0
 
 
-class laser:
-    pass
+class laser(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = laser_png
+        self.rect = self.image.get_rect()
+        # self.rect.center = (screen_width / 2, screen_height / 2)
+        self.image = pygame.transform.rotate(pygame.transform.rotate(laser_png, -3), 455)
 
 
 class game_settings:
@@ -49,11 +58,13 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Space Invaders")
-    clock = pygame.time.Clock()
+    fps = pygame.time.Clock()
+
     all_sprites = pygame.sprite.Group()
     spaceship = space_ship()
+    fireball = laser()
+    all_sprites.add(fireball)
     all_sprites.add(spaceship)
-
     running = True
     while running:
         for event in pygame.event.get():
@@ -64,12 +75,11 @@ def main():
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-
         all_sprites.update()
         screen.blit(game_background, (0, 0))
         all_sprites.draw(screen)
         pygame.display.flip()
-        clock.tick(30)
+        fps.tick(60)
 
 
 main()
